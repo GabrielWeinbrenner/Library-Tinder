9f6838aa165b2d4dbefe45d9788c0bdb855a88f5
Gabriel Weinbrenner <GabrielWeinbrenner@Gabriels-MacBook-Pro.local>
2020-02-08 16:09:21 -0500

    Dislike&Like;Images;API

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 import Currentbook from './booksView/Currentbook';
+import cross from '../images/x.png';
+import heart from '../images/heart.png';
 class App extends React.Component{
   state = {
     books: null,
     typeVal: "",
     type: "", 
-    currentBookValue: 0
+    currentBookValue: 0,
+    liked: [],
   }
   getBook(type){
     if(type.indexOf(" ") === -1){
@@ -28,15 +31,31 @@ class App extends React.Component{
     console.log("Submitted");
   }
   handleTypeChange = (event) =>{
-
       this.setState({
           typeVal: event.target.value
       })
   }
+  onDislikeClick = () => {
+    const a = this.state.currentBookValue+1;
+    this.setState({currentBookValue: a})
+  }
+  onLikeClick = () => {
+    const b = this.state.books[this.state.currentBookValue];
+    const booksLiked = this.state.liked.push(b);
+    const a = this.state.currentBookValue+1;
+    this.setState({currentBookValue: a, liked: booksLiked})
+
+
+  }
   handleOutput = () => {
     if(this.state.books !== null){
       return (
-        <Currentbook currentBookValue={this.state.currentBookValue} books={this.state.books} />
+        <div>
+          <Currentbook currentBookValue={this.state.currentBookValue} books={this.state.books} />
+          <div class="row alignButtons">
+            <img onClick={this.onDislikeClick} className="cross img-responsive" src={cross} alt='alternate'/>
+            <img onClick={this.onLikeClick} className="heart img-responsive" src={heart} alt='alternate'/>          </div>
+          </div>
       )           
     }else{
       return(
--- src/components/booksView/Currentbook.js
+++ src/components/booksView/Currentbook.js
@@ -3,10 +3,14 @@ import React from 'react';
 class Currentbook extends React.Component{
 
     render(){
-        console.log(this.props.books)
+        const currentBook = this.props.books[this.props.currentBookValue]
         return(
             <div className="container book">
-                <img alt="" src={this.props.books[this.props.currentBookValue].volumeInfo.imageLinks.thumbnail} />
+                <img alt="" src={currentBook.volumeInfo.imageLinks.thumbnail} />
+                <h5>{currentBook.volumeInfo.title}</h5>
+                <h6>by {currentBook.volumeInfo.authors[0]}</h6>
+                <p>{currentBook.volumeInfo.description}</p>
+
             </div>
         );
     }
